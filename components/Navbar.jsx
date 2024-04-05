bdfba7460025dd264d5e591c09050655f95c1ca6
Sachin Semwal <sachinsemwal18@gmail.com>
2024-04-05 15:44:14 +0530

    Done Cards and About Midhaguru

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,9 +33,12 @@ const Navbar = () => {
                         <NavItem to="https://edu.midha.guru/s/store?redirectToMicroFE=true" name="Courses" />
                         <NavItem to="https://midha.guru/contact-us/" name="Contact Us" />
                         <NavItem to="https://midha.guru/founder-profile/" name="Founder's Profile" />
+                        <Link href='https://edu.midha.guru/s/store?redirectToMicroFE=true' className="text-[1.2rem] font-[500] cursor-pointer">
                         <li className="relative group bg-[#6A1C1A] hover:bg-[#BE4E1E] rounded-3xl py-2 px-9 text-white text-[20px] transition duration-300 flex-shrink-0">
-                            <Link href='https://edu.midha.guru/s/store?redirectToMicroFE=true' className="text-[1.2rem] font-[500] cursor-pointe">Log in</Link>
+                            Log in
                         </li>
+                        
+                        </Link>
                     </ul>
                     {/* Responsive */}
                     <ul className={`md:hidden w-[50%] sm:w-[40%] md-[30%] h-auto fixed bg-[#ecebea]  backdrop-filter backdrop-blur-lg bg-opacity-95 top-[60px] 
