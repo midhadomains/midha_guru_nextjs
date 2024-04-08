b4fff5a09b4029d8dba44f955e765cbbf55ae912
niraj1412 <14nirajkumar@gmail.com>
2024-04-08 13:37:40 +0530

    updated homepage part 5

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,59 @@
 "use client"
-import Link from 'next/link'
-import React, { useState } from "react";
-import Image from 'next/image'
-import { IoMenu, IoClose } from "react-icons/io5";
+import Link from 'next/link';
+import React, { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { IoClose } from 'react-icons/io5';
+import { CgMenuLeftAlt } from 'react-icons/cg';
+import { MdCall } from 'react-icons/md';
+
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
+    const navbarRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (navbarRef.current && !navbarRef.current.contains(event.target)) {
+                setToggle(false);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
-        <nav className="mx-auto sticky top-0 z-10 bg-[#FFFDF6] shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+        <nav
+            ref={navbarRef}
+            className="mx-auto sticky top-0 z-10 bg-[#FFFDF6] w-full shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
             <div className='max-w-full '>
-                <div className="flex items-center justify-between w-[100%] h-[55px] md:h-[70px] max-w-[2500px] mx-auto">
-                    <Link href='/'><Image width={250} height={40} className='ml-4  md:ml-9  md:item-left w-[200px] h-[30px] lg:w-fit lg:h-[40px] ' src="/Midhaguru.svg" alt="MidhaFin Logo" /></Link>
-                    {toggle ?
-                        <IoClose onClick={() => setToggle(!toggle)} className="text-3xl md:hidden block mr-2" />
-                        : <IoMenu onClick={() => setToggle(!toggle)} className="text-3xl md:hidden block mr-2" />}
+                <div className="flex items-center justify-between  w-[100%] h-[55px] md:h-[70px] max-w-[2500px] mx-auto">
+                    <Link href='/'>
+                        <Image
+                            width={250}
+                            height={40}
+                            className='ml-1 sm:ml-4 md:item-left w-[200px] h-[30px] lg:w-fit lg:h-[40px] '
+                            src="/Midhaguru.svg"
+                            alt="Midhaguru Logo"
+                        />
+                    </Link>
+                    <div className='flex'>
+                        <Link href="tel:+91 95061 25061" className='m-auto w-fit'>
+                            <MdCall className='block md:hidden m-auto text-[#6A1C1A] text-[25px] ' />
+                        </Link>
+                        <Link href='https://edu.midhafin.com/s/authenticate' className='block md:hidden bg-[#6A1C1A] py-1 px-5 rounded-3xl text-[#F9ECC7] mx-4 max-w-[100px] w-fit'>Login</Link>
+                        {toggle ? (
+                            <IoClose
+                                onClick={() => setToggle(!toggle)}
+                                className="text-3xl md:hidden block mr-2 text-[#9A391D]"
+                            />
+                        ) : (
+                            <CgMenuLeftAlt
+                                onClick={() => setToggle(!toggle)}
+                                className="text-3xl md:hidden block mr-2 text-[#9A391D]"
+                            />
+                        )}
+                    </div>
 
                     <ul className="hidden md:flex  justify-around flex-nowrap  max-w-[1000px] w-[100%]">
                         <NavItem to='/' name='Home' />
@@ -29,27 +70,33 @@ const Navbar = () => {
                                 </div>
                             </div>
                         </li> */}
-                        <NavItem to="/about-us" name="About" />
+
+                        <NavItem to="https://edu.midhaguru.com/s/store" name="About" />
                         <NavItem to="https://edu.midha.guru/s/store?redirectToMicroFE=true" name="Courses" />
-                        <NavItem to="/contact-us/" name="Contact Us" />
-                        <NavItem to="/founders-profile/" name="Founder's Profile" />
-                        <Link href='https://edu.midha.guru/s/store?redirectToMicroFE=true' className="text-[1.2rem] font-[500] cursor-pointer">
-                        <li className="relative group bg-[#6A1C1A] hover:bg-[#BE4E1E] rounded-3xl py-2 px-9 text-white text-[20px] transition duration-300 flex-shrink-0">
-                            Log in
-                        </li>
-                        
+                        <NavItem to="/contact-us" name="Contact Us" />
+                        <NavItem to="/founders-profile" name="Founder's Profile" />
+                        <Link href='https://edu.midha.guru/s/authenticate' className="text-[1.2rem] font-[500] cursor-pointe">
+                            <li className="relative group bg-[#6A1C1A] hover:bg-[#BE4E1E] rounded-3xl py-2 px-9 text-white text-[20px] transition duration-300 flex-shrink-0">
+                                Log In
+                            </li>
                         </Link>
                     </ul>
                     {/* Responsive */}
-                    <ul className={`md:hidden w-[50%] sm:w-[40%] md-[30%] h-auto fixed bg-[#ecebea]  backdrop-filter backdrop-blur-lg bg-opacity-95 top-[60px] 
-                     ${toggle ? "right-[0]" : "hidden"}`}>
-                        <NavItem to='/' name='Home' />
+                    <ul
+                        className={`md:hidden w-[50%] sm:w-[40%] md:w-[40%] h-auto fixed bg-white top-[60px]  shadow-2xl
+                     ${toggle ? 'right-[0]' : 'hidden'}`}
+                    >
+                        <SmNavItem to='/' name='Home' />
+                        <hr />
                         {/* <NavItem to="/blog" name="Blogs" /> */}
-                        <NavItem to="https://midha.guru/about/" name="About" />
-                        <NavItem to="https://edu.midha.guru/s/store?redirectToMicroFE=true" name="Courses" />
-                        <NavItem to="https://midha.guru/contact-us/" name="Contact Us" />
-                        <NavItem to="https://midha.guru/founder-profile/" name="Founder's Profile" />
-                        <NavItem to="https://edu.midha.guru/s/store?redirectToMicroFE=true" name="Login" />
+                        <SmNavItem to="/about-us" name="About" />
+                        <hr/>
+                        <SmNavItem to="https://edu.midha.guru/s/store?redirectToMicroFE=true" name="Courses" />
+                        <hr />
+                        <SmNavItem to="/contact-us" name="Contact Us" />
+                        <hr />                       
+                        <SmNavItem to="/founders-profile" name="Founder's Profile" />
+                        <hr />
                     </ul>
                 </div>
             </div>
@@ -62,11 +109,21 @@ const NavItem = ({ to, name }) => {
         <li className="relative group  p-4 md:p-0 my-auto">
             <Link
                 href={to}
-                className="text-[1.1rem] font-[500] cursor-pointe pb-2 px-1 hover:text-[#BE4E1E]  transition duration-300 ">
+                className="text-[1.1rem] font-[500] cursor-pointer pb-2 px-1 hover:text-[#BE4E1E]  transition duration-300 ">
+                {name}
+            </Link>
+        </li>
+    );
+};
+const SmNavItem = ({ to, name }) => {
+    return (
+        <li className="relative group  p-4 md:p-0 my-auto text-[#6A1C1A]">
+            <Link
+                href={to}
+                className="text-[1.1rem] font-[500] cursor-pointer pb-2 px-1 hover:text-[#BE4E1E]  transition duration-300 ">
                 {name}
             </Link>
         </li>
     );
 };
 export default Navbar;
-
