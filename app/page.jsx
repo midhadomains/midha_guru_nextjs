f3a8fa76c5f62dc1d1610fbfdd68ea6c0752b6c8
Sachin Semwal <sachinsemwal18@gmail.com>
2024-04-08 13:19:14 +0530

    Upadted Navbar and cards

--- app/page.jsx
+++ app/page.jsx
@@ -2,15 +2,21 @@ import Image from "next/image";
 import Card from "../components/Card.jsx";
 import Choose from "@/components/Choose.jsx";
 import About from "../components/About.jsx";
+import Navbar from "@/components/Navbar.jsx";
+import Footer from "@/components/Footer.jsx";
 
 
 export default function Home() {
   return (
-    <div className="sm:p-8">
+    <>
+    <Navbar/>
+    <div className="">
     <About/>
     <Choose/>
     
     </div>
+    <Footer/>
+    </>
   );
 }
 
