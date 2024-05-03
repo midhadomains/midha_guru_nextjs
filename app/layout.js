f2a876546014e7c6c6a9b3507eeb40aa8b612afc
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-05-03 17:37:30 +0530

    google tag manager added

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+         {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YYJYHRV8EM"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YYJYHRV8EM');
+        </script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
