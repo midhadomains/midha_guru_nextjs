32dc485aee43cdccc919636ab76944e0207fcc66
midhadomains <159537481+midhadomains@users.noreply.github.com>
2024-06-12 12:14:05 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" style={{scrollBehavior:"smooth"}}>
+  <head>
+  <meta name="google-site-verification" content="oQoGSnBMdxfmwcRetmX71jj_DzPC4lyUX7XC9qbOHx4" />
+  <head/>
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId="G-YYJYHRV8EM" />
     </html>
